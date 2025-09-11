b0a57b7f1bd0d9e162d057f8c0ed509646af832a
miguel_borges <miguelborgess2016@gmail.com>
2025-09-11 15:14:12 -0300

    .

--- screens/PerfilResponsavel.js
+++ screens/PerfilResponsavel.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function App() {
+export default function PerfilScreen() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>perfil responsavel</Text>
-      <StatusBar style="auto" />
+      
+      <LinearGradient
+        colors={["#000000ff", "#780b47"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.header}
+      >
+        <Text style={styles.headerText}>PERFIL</Text>
+      </LinearGradient>
+
+      
+      <View style={styles.avatarContainer}>
+        <Ionicons name="person-circle-outline" size={100} color="black" />
+      </View>
+
+    
+      <Text style={styles.sectionTitle}>INFORMAÇÕES GERAIS</Text>
+
+      
+      <TouchableOpacity style={styles.option}>
+        <MaterialIcons name="edit" size={24} color="black" />
+        <Text style={styles.optionText}>Editar perfil.</Text>
+      </TouchableOpacity>
+
+    
+      <View style={styles.option}>
+        <Ionicons name="moon-outline" size={24} color="black" />
+        <Switch value={darkMode} onValueChange={setDarkMode} />
+        <Text style={styles.optionText}>Modo escuro.</Text>
+      </View>
+
+      
+      <TouchableOpacity style={styles.option}>
+        <Ionicons name="happy-outline" size={24} color="black" />
+        <Text style={styles.optionText}>Gerenciar criança.</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +51,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  header: {
+    height: 80,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 10,
+    width: "100%",
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  avatarContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    marginLeft: 20,
+  },
+  optionText: {
+    marginLeft: 10,
+    fontSize: 16,
   },
 });
